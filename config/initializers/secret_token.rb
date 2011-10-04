68991ead32508ed505ac043b6a6e67cec4a0de2c
javomorales <JavierMorales@javier-de-la-torres-macbook-pro-15.local>
2011-10-04 23:08:05 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Placebook::Application.config.secret_token = '6d08a3f2e7c3acc578a5fbe28e975b7465bb5ebbb7567a18d3283f347268d88df170bca37e8ec5af0c34b3e1e863f1f524058f7ea33de0207b0e454401d0f93c'
